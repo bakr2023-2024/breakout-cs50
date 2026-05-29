e811a213fcd79fb8ce8eda58241a8e5424cb3987
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-29 23:26:46 +0300

    feat: display FPS

--- main.lua
+++ main.lua
@@ -70,5 +70,12 @@ function love.draw(dt)
 
 	love.graphics.draw(textures["background"], 0, 0, 0, backgroundSX, backgroundSY)
 	gsm:render()
+	showFPS()
 	push:finish()
+end
+function showFPS()
+	love.graphics.setFont(fonts["small"])
+	love.graphics.setColor(0, 1, 0, 1)
+	love.graphics.print(tostring(love.timer.getFPS()), 5, 5)
+	love.graphics.setColor(1, 1, 1, 1)
 end
